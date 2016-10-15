eb344b7b48b5a4e7fe67aa0f2b92305c3d2021c0
84763 <84763@DESKTOP-OHS3PAE>
2016-10-15 19:27:56 +0800

    添加项目文件。

---
+++ App1/Class2.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App1
+{
+    class Class2
+    {
+        public static async Task<Stream> GetWebString(string url, string formData)
+        {
+            string requestUri = "";
+            if (formData.Contains("="))
+            {
+                requestUri = string.Concat(new object[]
+                {
+                    url,
+                    "?",
+                    formData,
+                    "r=",
+                    DateTime.Now.Ticks
+                });
+            }
+            else
+            {
+                if (string.IsNullOrEmpty(formData))
+                {
+                    requestUri = url + "?r=" + DateTime.Now.Ticks;
+                }
+                else
+                {
+                    requestUri = url + formData;
+                }
+            }
+            Stream result;
+            try
+            {
+                using (HttpClient httpClient = new HttpClient())
+                {
+                    using (HttpResponseMessage httpResponseMessage = await httpClient.GetAsync(new Uri(requestUri)))
+                    {
+                        httpResponseMessage.EnsureSuccessStatusCode();
+                        Stream text = await httpResponseMessage.Content.ReadAsStreamAsync();
+                        result = text;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                ex.ToString();
+                result = null;
+            }
+
+            return result;
+        }
+
+    }
+}
---
+++ App1/MainPage.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System.Xml;
+using Windows.UI.Xaml.Controls;
+using System.Collections.Generic;
+
+//“空白页”项模板在 http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409 上有介绍
+
+namespace App1
+{
+    /// <summary>
+    /// 可用于自身或导航至 Frame 内部的空白页。
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            getacg();
+        }
+
+        //private string getacg()
+        //{
+        //    //string url = "https://yande.re/post.xml?limit=1";
+        //    //WebRequest HttpWReq = (HttpWebRequest)WebRequest.Create(url);
+        //    //HttpWReq.Method = "GET";//GET请求
+        //    //                        //string x = await HttpClient.g 
+        //    //XmlDocument xmldoc = new XmlDocument();
+        //    //xmldoc.LoadXml(url);
+        //    //HttpWebResponse HttpWResp = (HttpWebResponse)HttpWReq.GetResponse();
+        //    //StreamReader sw = new StreamReader(HttpWResp.GetResponseStream());
+        //    ////将返回的结果读入流
+
+
+        //    //WebRequest myWebRequest = WebRequest.Create(url);
+
+        //    ////Send the 'WebRequest' and wait for response.
+        //    //WebResponse myWebResponse = myWebRequest.GetResponse();
+        //    //Stream strm = myWebResponse.GetResponseStream();
+        //    //StreamReader reader = new StreamReader(strm);
+        //    //string str = reader.ReadToEnd();
+
+        //    //      request.BeginGetResponse(new AsyncCallback(GetResponse), request);
+        //    //}
+
+
+
+        //    //void GetResponse(IAsyncResult res)
+        //    //{
+        //    //    HttpWebRequest request = (HttpWebRequest)res.AsyncState;
+        //    //    HttpWebResponse response = (HttpWebResponse)request.EndGetResponse(res);
+        //    //    string content = "";
+        //    //    using (StreamReader reader = new StreamReader(response.GetResponseStream(), Encoding.UTF8))
+        //    //    {
+        //    //        content = reader.ReadToEnd();
+        //    //    }
+        //    //    Deployment.Current.Dispatcher.BeginInvoke(delegate
+        //    //    {
+        //    //        MessageBox.Show(content);
+        //    //    });
+
+        //    //
+
+        //    //
+
+        //}
+
+        public async void getacg()
+        {
+            string url = "https://yande.re/post.xml?limit=1";
+            var mystring = await Class2.GetWebString(url, null);
+           
+            XmlDocument doc = new XmlDocument();
+            doc.Load(mystring);
+            var fcc = doc.ChildNodes;
+            XmlNodeList schoolNodeList = fcc;
+            schoolNodeList.GetEnumerator();
+            textb.Text = schoolNodeList.ToString();
+        }
+
+
+
+
+    }
+}
